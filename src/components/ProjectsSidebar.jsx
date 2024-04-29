74b59e44daa70891d462cfe548e754c079f81e13
bharath <bharath.r@idp.com>
2024-04-29 09:46:17 +0530

    some css classes added

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,9 @@
+export default function ProjectsSidebar() {
+    return (
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounder-r-xl">
+        <h2 className="">Your Projects</h2>
+        <div>
+            <button>+ Add project</button>
+        </div>
+    </aside>);
+}
